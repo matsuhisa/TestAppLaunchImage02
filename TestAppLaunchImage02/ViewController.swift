266fb13e8c5d5173fa3726cb4e0e34691bc48e81
Hironobu Matsuhisa <matsuhisa@gmail.com>
2015-02-14 23:46:36 +0900

    Initial Commit

---
+++ TestAppLaunchImage02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestAppLaunchImage02
+//
+//  Created by 松久浩伸 on 2015/02/14.
+//  Copyright (c) 2015年 matsuhisa.hironobu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
